a54198253e30ff2a6186b69ab26d6acbb5a25c50
Bandit1984 <1122005@est.intec.edu.do>
2025-12-24 18:05:26 -0400

    Being able to visualize the runs to see what I'm missing

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const buildStocks = require('./scripts/BuildStocks');
 
 // Default run: change concurrency/start/end as needed. This will actually launch browsers when run.
 if (require.main === module) {
-	const options = { concurrency: 35, start: 319, end: 1159};
+	const options = { concurrency: 40, start: 1131, end: 1451};
 	buildStocks(options).catch(err => {
 		console.error('BuildStocks failed:', err && err.message ? err.message : err);
 		process.exit(1);
