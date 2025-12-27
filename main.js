fdd4adb61480cbd54aeb6fd816cee9bd68370e80
Bandit1984 <1122005@est.intec.edu.do>
2025-12-27 08:22:43 -0400

    PC run

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const buildStocks = require('./scripts/BuildStocks');
 
 // Default run: change concurrency/start/end as needed. This will actually launch browsers when run.
 if (require.main === module) {
-	const options = { concurrency: 40, start: 4754, end: 4754+960};
+	const options = { concurrency: 100, start: 7990, end: 7990+2700};
 	buildStocks(options).catch(err => {
 		console.error('BuildStocks failed:', err && err.message ? err.message : err);
 		process.exit(1);
